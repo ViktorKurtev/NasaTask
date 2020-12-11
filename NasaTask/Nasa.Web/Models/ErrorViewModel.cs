5e148c554ae2f9fdec280f5ceda1c9cf8f020063
Viktor Kurtev <viktorkurtev@yahoo.com>
2020-12-11 17:03:53 +0200

    Initial Project Commit

---
+++ NasaTask/Nasa.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nasa.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
